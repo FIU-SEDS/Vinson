7a1c73f9b4626c585a6b3c2f33872adb1e7cacc3
Eriel Cabrera <erielcabrera1234@gmail.com>
2025-01-19 21:41:37 -0500

    .\src\main.cpp added SPI test

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,39 @@
 #include <SPI.h>
-#include <SD.h>
 #include <Adafruit_I2CDevice.h>
 
-const int chipSelect = 10; // Pin for CS (Chip Select)
+const int chipSelect = 10;
 
-void setup()
-{
-  // Start the serial communication
+void setup() {
   Serial.begin(115200);
-
-  // Initialize SPI and SD card
-  if (!SD.begin(chipSelect))
-  {
-    Serial.println("Initialization failed!");
-    return;
+  while (!Serial) {
+    ; // Wait for Serial Monitor
   }
-  Serial.println("SD card initialized.");
+
+  Serial.println("Starting SPI test...");
+
+  SPI.begin();
+  pinMode(chipSelect, OUTPUT);
+  digitalWrite(chipSelect, HIGH); // Deactivate CS initially
+
+  // Test with slower clock and specific SPI mode
+  SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE0));
+
+  digitalWrite(chipSelect, LOW); // Activate the device
+  delay(10);
+
+  Serial.println("Sending test command...");
+  uint8_t response = SPI.transfer(0x55); // Send a different test byte
+
+  digitalWrite(chipSelect, HIGH); // Deactivate the device
+  delay(10);
+
+  Serial.print("Response from device: 0x");
+  Serial.println(response, HEX);
+
+  SPI.endTransaction();
+  SPI.end();
 }
 
-void loop()
-{
-  // Example: Write to a file
-  File dataFile = SD.open("test.txt", FILE_WRITE);
-  if (dataFile)
-  {
-    dataFile.println("Hello, SD card!");
-    dataFile.close();
-    Serial.println("Data written to SD card.");
-  }
-  else
-  {
-    Serial.println("Error opening file.");
-  }
-  delay(1000);
+void loop() {
+  // No need to loop for this test
 }
