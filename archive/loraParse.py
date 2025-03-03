ac1c761f87564901b4fafad160786ef0a3cf9e0c
Eriel Cabrera <erielcabrera1234@gmail.com>
2025-03-03 18:59:46 -0500

    test

--- archive/loraParse.py
+++ archive/loraParse.py
@@ -3,15 +3,14 @@ import serial
 import time
 
 #setting up a serial connection (port and baud rate)
-ser = serial.Serial('/dev/cu.usbmodem11101', 9600, timeout=1) 
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) 
 time.sleep(2)
-
 
 output_csv = "parsed_data.csv"
 
 with open(output_csv, mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["Barometer", "Accelerator", "RSSI", "Signal"])
+    writer.writerow(["Altitude", "Acceleration", "RSSI", "Signal-to-Noise"])
 
 print("Waiting for LoRa data...")
 #reads the data file and processes each line 
@@ -22,24 +21,17 @@ try:
             clean_data = line.replace("Received: +RCV=", "")
             data_values = clean_data.split(',')
 
-            if len(data_values) >= 4:  # Ensuring there's enough data
-                altitude = data_values[0]  # Altitude 
-                rssi = data_values[-2]  # RSSI 
-                signal = data_values[-1]  # Signal 
-
-                barometer_accelerometer = data_values[1].split('/')  # Split by '/'
-                
-                if len(barometer_accelerometer) == 2:
-                    barometer, accelerometer = barometer_accelerometer
-                else:
-                    barometer, accelerometer = "N/A", "N/A"  # handling format errors
+            altitude = data_values[2]  # Altitude
+            accelerometer = data_values[3] 
+            rssi = data_values[4]  # RSSI 
+            signal_to_noise = data_values[5]  # Signal 
 
                 
-                with open(output_csv, mode='a', newline='') as csvfile:
+            with open(output_csv, mode='a', newline='') as csvfile:
                     writer = csv.writer(csvfile)
-                    writer.writerow([altitude, rssi, signal, barometer, accelerometer])
+                    writer.writerow([altitude, accelerometer, rssi, signal_to_noise])
 
-                print(f"Logged data: Altitude={altitude}, RSSI={rssi}, Signal={signal}, Barometer={barometer}, Accelerometer={accelerometer}")
+            print(f"Logged data: Altitude={altitude}, Accelerometer={accelerometer}, RSSI={rssi}, Signal={signal_to_noise}, ")     
 
 except KeyboardInterrupt:
     print("\nStopped receiving data.")
